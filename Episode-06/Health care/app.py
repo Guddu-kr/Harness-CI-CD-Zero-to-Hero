fb8e544615398f9af218a6cdd9b7711ae8f8605b
Arumulla Yaswanth Reddy <164642237+arumullayaswanth@users.noreply.github.com>
2026-07-29 00:12:29 +0530

    Update app.py

--- Episode-06/Health care/app.py
+++ Episode-06/Health care/app.py
@@ -38,5 +38,5 @@ def services():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
-    #app.run(host='0.0.0.0', port=3000, debug=False)
+    #app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=3000, debug=False)
