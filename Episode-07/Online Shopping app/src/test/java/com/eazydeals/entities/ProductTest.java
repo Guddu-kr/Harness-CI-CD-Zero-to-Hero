393c50629053214a5301bdf69c70b4945647fadd
Arumulla Yaswanth Reddy <yaswanth.arumulla@gmail.com>
2026-07-30 13:06:31 +0530

    Fix: use correct method name productQunatity (typo in original code)

--- Episode-07/Online Shopping app/src/test/java/com/eazydeals/entities/ProductTest.java
+++ Episode-07/Online Shopping app/src/test/java/com/eazydeals/entities/ProductTest.java
@@ -11,19 +11,21 @@ class ProductTest {
         product.setProductId(1);
         product.setProductName("Test Product");
         product.setProductPrice(99.99f);
-        product.setProductQuantity(10);
+        product.setProductQunatity(10);
 
         assertEquals(1, product.getProductId());
         assertEquals("Test Product", product.getProductName());
         assertEquals(99.99f, product.getProductPrice());
-        assertEquals(10, product.getProductQuantity());
+        assertEquals(10, product.getProductQunatity());
     }
 
     @Test
     void testProductDiscount() {
         Product product = new Product();
+        product.setProductPrice(100.0f);
         product.setProductDiscount(15);
 
         assertEquals(15, product.getProductDiscount());
+        assertEquals(85, product.getProductPriceAfterDiscount());
     }
 }
